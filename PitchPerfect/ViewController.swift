9cb66b2e63287cf539920f39705968ec4c4ea044
Tianrui Zhang <jameschang1992@gmail.com>
2019-03-10 00:21:50 +0800

    Initial Commit

---
+++ PitchPerfect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by James Chang on 2019/3/10.
+//  Copyright © 2019 James Chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
