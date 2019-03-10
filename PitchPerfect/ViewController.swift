144ff5b7ec4255c98cf861767d46f07dd25deb08
Tianrui Zhang <jameschang1992@gmail.com>
2019-03-11 00:40:50 +0800

    Adjust the size and position of record button and stop recording button. Add asset files.

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+    }
 
-
+    @IBAction func recordAudio(_ sender: Any) {
+        print("Record button was pressed.")
+        recordingLabel.text = "Recording in Progress..."
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        print("Stop recording button was pressed")
+    }
 }
 
